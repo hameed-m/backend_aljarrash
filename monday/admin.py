cb08ef133379fba751ff13711049ff5c2812d200
Hameed M <s201424820@gmail.com>
2024-01-16 00:21:57 +0300

    clear cache

--- monday/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Client, Project, Comment
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Client)
-admin.site.register(Project)
-admin.site.register(Comment)
