4d4c786f71632424039b8c2e7ad1cf7b2a983100
Hameed <a-alhafufi@outlook.sa>
2024-01-22 15:47:16 +0300

    handle none value

--- monday/serializers.py
+++ monday/serializers.py
@@ -24,12 +24,18 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = ["stage","project_name","design_eng","architectural_status","structural_status","plumbing_status","electrical_status","client_number","sketch_approval_date","columns_approval_date","typeof_follow_up","investor_affiliation","project_receipt_date","project_type","land_number","land_area","project_location","project_number","sketch_design_progress_status","structural_eng","structural_design_start_date","structural_review","structural_delivery_date","electrical_eng","electrical_design_start_date","electrical_delivery_date","architectural_drawing_start_date","architectural_delivery_date","plumbing_design_start_date","plan_delivery_date","modification_price","created_at","moved_at","deed","identity","land_survey","soil_test","client_form","old_license","civil_defense", "water_authority", "design_eng_name", "structural_eng_name", "electrical_eng_name"]
 
     def get_design_eng_name(self, obj):
+        if(obj.design_eng == None):
+            return None
         return obj.design_eng.first_name
     
     def get_structural_eng_name(self, obj):
+        if(obj.structural_eng == None):
+            return None
         return obj.structural_eng.first_name
     
     def get_electrical_eng_name(self, obj):
+        if(obj.electrical_eng == None):
+            return None
         return obj.electrical_eng.first_name
 
 class CommentSerializer(serializers.ModelSerializer):
