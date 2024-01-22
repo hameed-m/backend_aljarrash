2e9522c03759a23d49ccf0606393b3d3600c8e2a
Hameed <a-alhafufi@outlook.sa>
2024-01-22 15:25:07 +0300

    add project moved_at, created_at

--- monday/serializers.py
+++ monday/serializers.py
@@ -14,10 +14,23 @@ class ClientSerializer(serializers.ModelSerializer):
         # fields = ["name"]
 
 class ProjectSerializer(serializers.ModelSerializer):
+    design_eng_name = serializers.SerializerMethodField()
+    structural_eng_name = serializers.SerializerMethodField()
+    electrical_eng_name = serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
-        fields = "__all__"
-        # fields = []
+        # fields = "__all__"
+        fields = ["stage","project_name","design_eng","architectural_status","structural_status","plumbing_status","electrical_status","client_number","sketch_approval_date","columns_approval_date","typeof_follow_up","investor_affiliation","project_receipt_date","project_type","land_number","land_area","project_location","project_number","sketch_design_progress_status","structural_eng","structural_design_start_date","structural_review","structural_delivery_date","electrical_eng","electrical_design_start_date","electrical_delivery_date","architectural_drawing_start_date","architectural_delivery_date","plumbing_design_start_date","plan_delivery_date","modification_price","created_at","moved_at","deed","identity","land_survey","soil_test","client_form","old_license","civil_defense", "water_authority", "design_eng_name", "structural_eng_name", "electrical_eng_name"]
+
+    def get_design_eng_name(self, obj):
+        return obj.design_eng.name
+    
+    def get_structural_eng_name(self, obj):
+        return obj.structural_eng.name
+    
+    def get_electrical_eng_name(self, obj):
+        return obj.electrical_eng.name
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
