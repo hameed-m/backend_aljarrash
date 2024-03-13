451fcd4dcdf64f715cb794b25c5a05426b3c539b
Hameed M <s201424820@gmail.com>
2024-03-13 20:32:04 +0300

    auth 6

--- monday/urls.py
+++ monday/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .views import HelloView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,4 +9,4 @@ router.register("projects", views.ProjectsViewSet)
 router.register("comments", views.CommentsViewSet)
 router.register("table_views", views.TableViewViewSet)
 
-urlpatterns = router.urls + []
+urlpatterns = router.urls + [path("hello/", HelloView.as_view(), name="hello")]
