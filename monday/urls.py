7554e895321c3df3df61b7b7a9c4e2efe0c3f406
Hameed M <s201424820@gmail.com>
2024-03-13 20:45:02 +0300

    auth 7

--- monday/urls.py
+++ monday/urls.py
@@ -9,4 +9,4 @@ router.register("projects", views.ProjectsViewSet)
 router.register("comments", views.CommentsViewSet)
 router.register("table_views", views.TableViewViewSet)
 
-urlpatterns = router.urls + [path("hello/", HelloView.as_view(), name="hello")]
+urlpatterns = router.urls
