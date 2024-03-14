5c7c12c7dfc1820b27adf6f52daf18ed603c1dea
Hameed M <s201424820@gmail.com>
2024-03-14 15:19:34 +0300

    auth 12

--- monday/urls.py
+++ monday/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import EmployeesViewSet, ClientsViewSet, ProjectsViewSet, CommentsViewSet, TableViewViewSet, HelloView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register("employees", EmployeesViewSet)
@@ -9,4 +10,7 @@ router.register("projects", ProjectsViewSet)
 router.register("comments", CommentsViewSet)
 router.register("table_views", TableViewViewSet)
 
-urlpatterns = router.urls + [path("hello/", HelloView.as_view(), name="hello")]
+urlpatterns = router.urls + [
+    path("hello/", HelloView.as_view(), name="hello"),
+    path("auth/", obtain_auth_token, name='api_token_auth'),
+]
